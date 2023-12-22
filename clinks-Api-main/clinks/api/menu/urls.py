6484ad66f3f71b8115111ed36292584e8a961fa1
Dorela Karaj <dorela@hotmail.co.uk>
2023-12-22 13:22:53 +0100

    Retrieve categories with passcode

--- clinks-Api-main/clinks/api/menu/urls.py
+++ clinks-Api-main/clinks/api/menu/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import Detail
+from .views import Detail, MenuCategoryDetails
 
 urlpatterns = [
     path('/<int:id>', Detail.as_view()),
+    path('/<int:id>/categories', MenuCategoryDetails.as_view()),
+
 ]
--- clinks-Api-main/clinks/api/menu_category/serializers.py
+++ clinks-Api-main/clinks/api/menu_category/serializers.py
@@ -1,12 +1,8 @@
-from ..utils.Serializers import serializers, CreateModelSerializer, EditModelSerializer, ListModelSerializer
-
-from ..utils import Constants
-from ..menu_item.serializers import MenuItemMenuListSerializer
-from ..category.serializers import CategoryListSerializer
-
-from ..company_member.models import CompanyMember, Company
-
 from .models import MenuCategory
+from ..category.serializers import CategoryListSerializer
+from ..company_member.models import CompanyMember, Company
+from ..menu_item.serializers import MenuItemMenuListSerializer
+from ..utils.Serializers import serializers, CreateModelSerializer, ListModelSerializer
 
 
 class MenuCategoryCreateSerializer(CreateModelSerializer):
@@ -40,7 +36,7 @@ class MenuCategoryCreateSerializer(CreateModelSerializer):
 
     def create(self, validated_data):
         last_menu_category = MenuCategory.objects.filter(menu=validated_data['menu']).order_by('-order').first()
-        order = last_menu_category.order+1 if last_menu_category else 0
+        order = last_menu_category.order + 1 if last_menu_category else 0
 
         validated_data["order"] = order
 
@@ -60,3 +56,11 @@ class MenuCategoryDetailSerializer(ListModelSerializer):
     def get_menu_items(self, instance):
         menu_items = instance.items.all()
         return MenuItemMenuListSerializer(menu_items, many=True).data
+
+
+class MenuCategoriesDetailSerializer(ListModelSerializer):
+    category = CategoryListSerializer()
+
+    class Meta:
+        model = MenuCategory
+        fields = "__all__"
