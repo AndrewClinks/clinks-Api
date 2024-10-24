83af0a4c898059ecdf865922dedaf452d88ab5cf
lindsay macvean <lindsaymacvean@gmail.com>
2024-10-24 22:11:39 +0100

    back to 1 logger

--- clinks-Api-main/clinks/api/test_order/views.py
+++ clinks-Api-main/clinks/api/test_order/views.py
@@ -12,26 +12,25 @@ import os
 logger = logging.getLogger('clinks-api-live')
 
 class CreateTestOrder(SmartAPIView):
-    # permission_classes = [IsAdminPermission]
-    # create_serializer = OrderCreateSerializer
+    permission_classes = [IsAdminPermission]
+    create_serializer = OrderCreateSerializer
 
     def get(self, request, venue_id, *args, **kwargs):
-        # venue_id = 1
-        # venue = Venue.objects.get(id=venue_id)
-        # menu_items = MenuItem.objects.filter(menu_id=venue_id) 
+        venue = Venue.objects.get(id=venue_id)
+        menu_items = MenuItem.objects.filter(menu_id=venue_id) 
 
         # Prepare the items (based on your example data from the query)
-        # items = [
-        #     {
-        #         "id": item.id,
-        #         "menu_item_id": item.id, 
-        #         "quantity": 1, 
-        #         "price": item.price
-        #     }
-        #     for item in menu_items
-        # ]
+        items = [
+            {
+                "id": item.id,
+                "menu_item_id": item.id, 
+                "quantity": 1, 
+                "price": item.price
+            }
+            for item in menu_items
+        ]
 
-        # # Create the address
+        # Create the address
         # address_data = {
         #     "latitude": 51.896791,  # Example latitude (Cork coordinates)
         #     "longitude": -8.470114,  # Example longitude (Cork coordinates)
@@ -45,34 +44,27 @@ class CreateTestOrder(SmartAPIView):
         # Some of this is alsy being set in the order serializer
         # It needs this initial data to get through initial validators
         # Set test data for required fields
-        # data = {
-        #     "customer": 7,  # Andrew Scannell
-        #     "venue": 1,
-        #     "items": items,  # The items pulled from the database for the venue
-        #     "address": 3, # 12 South Mall, Cork
-        #     "menu": venue.id,
-        #     "payment": {
-        #         "card": "1", # this is needed here and then again inside the serializer... I know... it's a bit weird
-        #         "method": "card",
-        #         "expected_price": sum(item['price'] for item in items),
-        #         "amount": sum(item['price'] for item in items),  # Calculate total amount from items
-        #         "currency": "EUR",
-        #         "status": "PENDING"
-        #     },
-        #     "status": 'PENDING',
-        #     "delivery_status": 'AWAITING',
-        #     "total_price": sum(item['price'] for item in items),  # Set total price
-        #     "order_date": timezone.now(),
-        # }
+        data = {
+            "customer": 7,  # Andrew Scannell
+            "venue": 1,
+            "items": items,  # The items pulled from the database for the venue
+            "address": 3, # 12 South Mall, Cork
+            "menu": venue.id,
+            "payment": {
+                "card": "1", # this is needed here and then again inside the serializer... I know... it's a bit weird
+                "method": "card",
+                "expected_price": sum(item['price'] for item in items),
+                "amount": sum(item['price'] for item in items),  # Calculate total amount from items
+                "currency": "EUR",
+                "status": "PENDING"
+            },
+            "status": 'PENDING',
+            "delivery_status": 'AWAITING',
+            "total_price": sum(item['price'] for item in items),  # Set total price
+            "order_date": timezone.now(),
+        }
 
-        data = []
-
-        if os.environ.get('RUN_MAIN') == 'true':
-            logger.info(f"TEST ORDER in main process: {data}")
-        else:
-            logger.info(f"TEST ORDER not in main process: {data}")
-
-        return Response(data, status=status.HTTP_200_OK)
+        logger.info(f"TEST ORDER starting for: {data}")
 
         # Validate and save the new order
         serializer = self.create_serializer(data=data, context={'is_test_order': True})
